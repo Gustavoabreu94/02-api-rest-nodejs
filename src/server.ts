552e52d855cc0452df3bf4447b614f1c12c83113
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-26 20:12:13 -0300

    createe trasactions route

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,13 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
-import { randomUUID } from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
+app.register(cookie);
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: randomUUID(),
-      title: "Transacao de teste",
-      amount: 1000,
-    })
-    .returning("*");
-
-  return transaction[0];
-});
-
-app.get("/gethello", async () => {
-  const transactions = await knex("transactions").select("*");
-
-  return transactions;
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
