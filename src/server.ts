c1b2d6e6e728defba2efb3c14f5f8108c4187fb7
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-26 10:07:21 -0300

    create db conection

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,31 @@
 import fastify from "fastify";
+import { randomUUID } from "node:crypto";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  return "Hello World";
+  const transaction = await knex("transactions")
+    .insert({
+      id: randomUUID(),
+      title: "Transacao de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction[0];
+});
+
+app.get("/gethello", async () => {
+  const transactions = await knex("transactions").select("*");
+
+  return transactions;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running!");
